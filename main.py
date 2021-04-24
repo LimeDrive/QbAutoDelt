e738461ce3a03b544ff804539a39d34a0b766279
LimeDrive <limecat@limedrive.eu>
2021-04-24 03:10:13 +0200

    add cat

--- main.py
+++ main.py
@@ -88,7 +88,10 @@ def scoreTorrent(cfg, qbt):
     tgprio = cfg["t_tags"]["priority"]
     tgpref = cfg["t_tags"]["prefer"]
     tgex = cfg["t_tags"]["exclud"]
-    tgstate = cfg["t_tags"]["states"]
+    tgstate = cfg["t_states"]["states"]
+    catprio = cfg["t_cats"]["priority"]
+    catpref = cfg["t_cats"]["prefer"]
+    catex = cfg["t_cats"]["exclud"]
     data = dict()
     for t in qbt.torrents_info():
         #logger.debug(t)
@@ -97,13 +100,15 @@ def scoreTorrent(cfg, qbt):
         s_ratio = -10000 if t.ratio < min_ratio else t.ratio * 100
         s_tag = 9999999 if t.tags in tgprio else 100000 if t.tags in tgpref else - \
             99999999999 if t.tags in tgex else 0
+        s_cat = 9999999 if t.category in catprio else 100000 if t.category in catpref else - \
+            99999999999 if t.category in catex else 0
         s_state = -99999999999 if t.state in tgstate else 0
-        t_score = s_ratio + s_seed + s_tag + s_state
+        t_score = s_ratio + s_seed + s_tag + s_state + s_cat
         t_info = (t.name, t.hash, t.size)
         data[t_info] = t_score
         logger.debug(f"\n \
             {t.name} :\n \
-            Ratio: {str(t.ratio)}/={str(s_ratio)}   SeedTime: {str(t.time_active)}/={str(s_seed)}   Tag: {t.tags}/={str(s_tag)}   State: {t.state}/={str(s_state)}\n \
+            Ratio: {str(t.ratio)}/={str(s_ratio)}   SeedTime: {str(t.time_active)}/={str(s_seed)}   Tag: {t.tags}/={str(s_tag)}   Category:  Tag: {t.category}/={str(s_cat)}   State: {t.state}/={str(s_state)}\n \
             Final Score: {str(t_score)}")
     logger.debug(f"Data update, torrent scored : \n" + str(data))
     return data
